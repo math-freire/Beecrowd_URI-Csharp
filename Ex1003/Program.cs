a228a48605ec38473377d8011b96881be63255dd
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-08 23:37:41 -0300

    Ex1003 - Soma simples

--- Ex1003/Program.cs
+++ Ex1003/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-Console.WriteLine("Hello, World!");
+class URI {
+    static void Main(string[] args) {
+
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        Console.WriteLine("SOMA = " + (x + y));
+    }
+}

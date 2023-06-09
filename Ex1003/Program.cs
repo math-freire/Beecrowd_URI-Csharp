57bc8439a946d9487c459d1940460447da9d4f37
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-09 00:00:54 -0300

    Ex1003 - Simple Sum

--- Ex1003/Program.cs
+++ Ex1003/Program.cs
@@ -12,7 +12,6 @@
 
 class URI {
     static void Main(string[] args) {
-
         int x = int.Parse(Console.ReadLine());
         int y = int.Parse(Console.ReadLine());
         Console.WriteLine("SOMA = " + (x + y));

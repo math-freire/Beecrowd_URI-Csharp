871ce828937db0b50351176aee9b33d89c44a786
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-09 00:09:35 -0300

    Ex1007 - Diferença entre produtos

---
+++ Ex1007/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*
+Read four integer values named A, B, C and D. Calculate and print the difference of product A and B by the product of C and D (A * B - C * D).
+
+Input
+The input file contains 4 integer values.
+
+Output
+Print DIFERENCA (DIFFERENCE in Portuguese) with all the capital letters, according to the following example, with a blank space 
+before and after the equal signal.
+*/
+
+class URI {
+
+    static void Main(string[] args) {
+
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+        int d = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("DIFERENCA = " + (a*b - c*d));
+
+    }
+
+}

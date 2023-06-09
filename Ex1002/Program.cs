c5f8f0710013180712afe8f7b38eb7d8f45abc5f
Matheus Freire <matheus.freire34@hotmail.com>
2023-06-08 23:59:00 -0300

    Ex1002 - Area do circulo

---
+++ Ex1002/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+/*
+ * The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+
+Calculate the area using the formula given in the problem description.
+
+Input
+The input contains a value of floating point (double precision), that is the variable R.
+
+Output
+Present the message "A=" followed by the value of the variable, as in the example bellow, with four places after the decimal point. 
+Use all double precision variables. Like all the problems, don't forget to print the end of line after the result, otherwise you will 
+receive "Presentation Error".
+ */
+
+class URI {
+
+    static void Main(string[] args) {
+
+        double r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        double pi = 3.14159;
+        double a = pi * Math.Pow(r, 2);
+
+        Console.WriteLine("A=" + a.ToString("F4", CultureInfo.InvariantCulture));
+
+    }
+
+}
--- Ex1003/Program.cs
+++ Ex1003/Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 
+
+//read two integer values, in this case, the variables a and b. after this, calculate the sum between them and assign it to the variable soma. 
+//write the value of this variable.
+//input
+//the input file contains 2 integer numbers.
+
+//output
+//print the message "soma" with all the capital letters, with a blank space before and after the equal signal followed by the corresponding 
+//value to the sum of a and b. like all the problems, don't forget to print the end of line, otherwise you will receive "presentation error"
+
 class URI {
     static void Main(string[] args) {
 
